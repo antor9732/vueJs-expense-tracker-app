5206443aafc703801e7862dfa50fba3959a6030e
Md. Mahmudul Hasan <antormahmud789@gmail.com>
2025-06-17 14:35:24 +0600

    expensise tracker Ui Design Comnplete

--- src/App.vue
+++ src/App.vue
@@ -1,2 +1,29 @@
+<script>
+import Header from './components/Header.vue';
+import Balance from './components/Balance.vue';
+import IncomeExpense from './components/IncomeExpense.vue';
+import AddTransaction from './components/AddTransaction.vue';
+import TransactionList from './components/TransactionList.vue';
 
-<template>My App</template>
+export default {
+    name: 'App',
+    components: {
+        Header,
+        Balance,
+        IncomeExpense,
+        AddTransaction,
+        TransactionList
+    }
+};
+</script>
+
+<template>
+    <div class="container">
+        <Header />
+        <Balance />
+        <IncomeExpense />
+        <TransactionList />
+        <AddTransaction />
+    </div>
+
+</template>
